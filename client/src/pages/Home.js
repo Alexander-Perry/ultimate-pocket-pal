0b97acd4f0c923a043b7dc8ecb6469d9b3a05c38
Alex <alexander.h.perry@gmail.com>
2022-10-21 16:46:45 +1100

    basic page layout completed, tabs working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,48 +1,86 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Auth from '../utils/auth';
-import { Grid, Button, Container } from '@mui/material';
+import { Button, Container, Tabs, Tab , Box} from '@mui/material';
+
+// let today = new Date();
+// console.log(today.getDay());
 
 
 const HomePage = () => {
+    const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const today = new Date().getDay();
+    const [tabIndex, setTabIndex] = useState(today);
+    const handleTabChange = (event, newTabIndex) => setTabIndex(newTabIndex); 
+
     // If not logged in, display 'intro' page, with login button.
-    // List 7 day Grid (cards), Monday - Sunday
+    // List 7 day tabs, Sunday - Sat
     // Each day show the users entries for that day
     // clicking the day allows adding and removing events
-    // 
+    //
+    // Future-> Date functions to build into proper calendar
 
-    const dayOfWeek = ['Monday', 'Tuesday', 'Wednesday','Thursday','Friday','Saturday','Sunday']
 
+   
+
+    
     return (
-        <>
+        <Container>
             {!Auth.loggedIn()
                 ? (
                     <div>Welcome Page</div>
                 )
                 : (
-                    <Container>
-                        <Grid
-                            container
-                            direction="column"
-                            justifyContent="flex-start"
-                            alignItems="flex-start"
-                            sx={{ p: 2 }}
-                        >
-                            {dayOfWeek.map((day) => {
-                                return (
-                                    <Grid item key={day}>{day}</Grid>
-                                )
-                                
-                            })}
-                            <Grid item></Grid>
-                            <Grid item>Costs</Grid>
+                    <Box display={'flex'} sx={{ bgcolor: '#888888' }}>
+                        <Box  >
+                            <Tabs value={tabIndex} orientation='vertical' onChange={handleTabChange}>
+                                {dayOfWeek.map((day, index) => {
+                                    return (
+                                        <Tab label={day} />
+                                    )
+                                })}
+                            </Tabs>
+                        </Box>
+                        <Box sx={{ margin: 2, display: 'flex' }}>
+                            {tabIndex === 0 && (
+                                <Box>
+                                    {tabIndex}
+                                </Box>
+                            )}
+                            {tabIndex === 1 && (
+                                <Box>
+                                    {tabIndex}
+                                </Box>
+                            )}
+                            {tabIndex === 2 && (
+                                <Box>
+                                    {tabIndex}
+                                </Box>
+                            )}
+                            {tabIndex === 3 && (
+                                <Box>
+                                    {tabIndex}
+                                </Box>
+                            )}
+                            {tabIndex === 4 && (
+                                <Box>
+                                    {tabIndex}
+                                </Box>
+                            )}
+                            {tabIndex === 5 && (
+                                <Box>
+                                    {tabIndex}
+                                </Box>
+                            )}
+                            {tabIndex === 6 && (
+                                <Box>
+                                    {tabIndex}
+                                </Box>
+                            )}
 
-                            <Grid item></Grid>
-
-
-                        </Grid>
-                    </Container>
+                        </Box>
+                    </Box>
                 )}
-        </>
+        </Container>
     )
 };
 
